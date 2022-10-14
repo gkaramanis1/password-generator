738d504870d933e8c7f9a0625903ec499a2263ee
George Karamanis <gkaramanis1@outlook.com>
2022-10-14 15:47:39 -0400

    added prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,35 @@
 // Assignment code here
+var passwordLength;
+var hasUppercase;
+var hasLowercase;
+var hasSymbol;
+var hasNumeric;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var lengthInput = window.prompt("Enter the length you want your password to be.");
 
-  passwordText.value = password;
+if (lengthInput > 128 || lengthInput >= 1) {
+window.alert("Accepted")
+}
+passwordLength = lengthInput;
+
+var hasUppercase = window.prompt()
+
+
+generatePassword();
+
+}
+
+function generatePassword() {
+alert(passwordLength);
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-  window.prompt("Enter the length you want your password to be.");
+
+ 
